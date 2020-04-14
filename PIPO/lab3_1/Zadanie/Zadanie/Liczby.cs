edea38892ab69c52b5b44b1f4b49d6030e3acbe0
MichalLaskawski <61832510+MichalLaskawski@users.noreply.github.com>
2020-04-14 20:54:46 +0200

    Add files via upload

---
+++ PIPO/lab3_1/Zadanie/Zadanie/Liczby.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zadanie
+{
+    public class Liczby
+    {
+        protected int a;
+        protected int b;
+        protected int c;
+
+        public int A
+            {
+                get { return a; }
+                set { a += 0; }
+            }
+        public int B
+        {
+            get { return b; }
+            set { b += 0; }
+        }
+        public int C
+        {
+            get { return c; }
+            set { c += 0; }
+        }
+    }
+}
---
+++ PIPO/lab3_1/Zadanie/Zadanie/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Liczby L1 = new Liczby();
+            Liczby L2 = new Liczby();
+
+            L1.A = 2;
+            L1.B = 3;
+            L1.C = 4;
+
+            L2.A = 5;
+            L2.B = 6;
+            L2.C = 7;
+
+            Lepsze_liczby liczby = new Lepsze_liczby();
+            liczby.liczby();
+
+            Lepsze_liczby LL1 = new Lepsze_liczby();
+
+        }
+    }
+
+}
